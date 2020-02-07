01e9354df2b6e52578f9261548e9ff32c74413e1
g00311302 <g00311302@gmit.ie>
2020-02-07 12:35:36 +0000

    test

---
+++ src/main/java/ie/gmit/CounterTest.java
@@ -0,0 +1,11 @@
+package ie.gmit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+public class CounterTest {
+
+    private Counter myCounter;
+    @
+}
